7546d56b0c32c799ad04cf6aa254fe0f1ebbe636
K-MHOON <azmria117@gmail.com>
2023-01-07 22:40:07 +0900

    Initial commit

---
+++ src/main/java/com/example/studyschedule/StudyScheduleApplication.java
@@ -0,0 +1,13 @@
+package com.example.studyschedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyScheduleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyScheduleApplication.class, args);
+    }
+
+}
