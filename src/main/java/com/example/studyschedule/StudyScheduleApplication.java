9a35f58bd14abe530755a174e8287e404893ddea
K-MHOON <azmria117@gmail.com>
2023-02-12 20:52:50 +0900

    feat: Spring Security 설정 및 Jwt Authentication Filter 추가 및 적용

--- src/main/java/com/example/studyschedule/StudyScheduleApplication.java
+++ src/main/java/com/example/studyschedule/StudyScheduleApplication.java
@@ -3,9 +3,7 @@ package com.example.studyschedule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @EnableJpaAuditing
