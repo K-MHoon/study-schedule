9608b89683b6aeed2996fbe38d3cd6e76bbd0606
K-MHOON <azmria117@gmail.com>
2023-04-08 15:48:40 +0900

    feat : 스터디 가입 정보 null 체크 추가

--- src/main/java/com/example/studyschedule/model/dto/study/StudyRegisterDto.java
+++ src/main/java/com/example/studyschedule/model/dto/study/StudyRegisterDto.java
@@ -1,12 +1,14 @@
 package com.example.studyschedule.model.dto.study;
 
 import com.example.studyschedule.entity.study.StudyRegister;
-import com.example.studyschedule.model.dto.member.MemberDto;
+import com.example.studyschedule.utils.DateUtils;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
+
+import java.util.Objects;
 
 @Builder(access = AccessLevel.PRIVATE)
 @Getter
@@ -14,20 +16,36 @@ import lombok.ToString;
 @ToString
 public class StudyRegisterDto {
 
+    private Long id;
+
     private String goal;
 
     private String objective;
 
     private String comment;
 
-    private MemberDto requestMember;
+    private String createdAt;
+
+    private String approvalAt;
+
+    private String approvalBy;
+
+    private String requestMemberId;
 
     public static StudyRegisterDto entityToDto(StudyRegister studyRegister) {
+        Objects.requireNonNull(studyRegister);
+        String approvalBy = studyRegister.getApprovalBy() == null ? null : studyRegister.getApprovalBy().getMemberId();
+        String approvalAt = studyRegister.getApprovalAt() == null ? null : DateUtils.localDateTimeToString(studyRegister.getApprovalAt());
+
         return StudyRegisterDto.builder()
+                .id(studyRegister.getId())
                 .goal(studyRegister.getGoal())
                 .objective(studyRegister.getObjective())
                 .comment(studyRegister.getComment())
-                .requestMember(MemberDto.entityToDtoSimple(studyRegister.getRequestMember()))
+                .requestMemberId(studyRegister.getRequestMember().getMemberId())
+                .createdAt(DateUtils.localDateTimeToString(studyRegister.getCreatedAt()))
+                .approvalAt(approvalAt)
+                .approvalBy(approvalBy)
                 .build();
     }
 }
