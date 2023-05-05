517196a63c303faddc0831f423cbe99297795cb7
K-MHOON <azmria117@gmail.com>
2023-05-05 19:34:25 +0900

    feat : Todo에 완료 여부, 비고, 완료 일자 기능 추가

--- src/main/java/com/example/studyschedule/model/dto/schedule/TodoDto.java
+++ src/main/java/com/example/studyschedule/model/dto/schedule/TodoDto.java
@@ -1,5 +1,6 @@
 package com.example.studyschedule.model.dto.schedule;
 
+import com.example.studyschedule.entity.schedule.ScheduleTodo;
 import com.example.studyschedule.entity.schedule.Todo;
 import com.example.studyschedule.enums.IsClear;
 import com.example.studyschedule.utils.DateUtils;
@@ -22,6 +23,9 @@ public class TodoDto {
     private String content;
     private String createdAt;
     private String updatedAt;
+    private IsClear isClear;
+    private String reason;
+    private String clearDate;
 
     public static TodoDto entityToDto(Todo todo) {
         return TodoDto.builder()
@@ -31,5 +35,25 @@ public class TodoDto {
                 .createdAt(DateUtils.localDateTimeToString(todo.getCreatedAt()))
                 .updatedAt(DateUtils.localDateTimeToString(todo.getUpdatedAt()))
                 .build();
+    }
+
+    public static TodoDto entityToDto(ScheduleTodo scheduleTodo) {
+        Todo todo = scheduleTodo.getTodo();
+        return TodoDto.builder()
+                .id(todo.getId())
+                .title(todo.getTitle())
+                .content(todo.getContent())
+                .createdAt(DateUtils.localDateTimeToString(todo.getCreatedAt()))
+                .updatedAt(DateUtils.localDateTimeToString(todo.getUpdatedAt()))
+                .isClear(scheduleTodo.getIsClear())
+                .reason(scheduleTodo.getReason())
+                .clearDate(getClearDate(scheduleTodo))
+                .build();
+    }
+
+    private static String getClearDate(ScheduleTodo scheduleTodo) {
+        return scheduleTodo.getIsClear() == IsClear.Y
+                ? DateUtils.localDateTimeToString(scheduleTodo.getUpdatedAt())
+                : null;
     }
 }
