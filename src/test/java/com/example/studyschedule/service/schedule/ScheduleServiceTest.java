9d6814f2d3a0c336fde011a44d49b0525ecf7a2a
K-MHOON <azmria117@gmail.com>
2023-07-15 20:08:22 +0900

    test : 스케줄 타입 추가에 따른 테스트 코드 작성

--- src/test/java/com/example/studyschedule/service/schedule/ScheduleServiceTest.java
+++ src/test/java/com/example/studyschedule/service/schedule/ScheduleServiceTest.java
@@ -6,22 +6,31 @@ import com.example.studyschedule.entity.schedule.Schedule;
 import com.example.studyschedule.entity.schedule.Todo;
 import com.example.studyschedule.entity.study.Study;
 import com.example.studyschedule.enums.IsUse;
+import com.example.studyschedule.enums.SchedulePeriod;
+import com.example.studyschedule.enums.ScheduleType;
 import com.example.studyschedule.model.dto.schedule.ScheduleDto;
 import com.example.studyschedule.model.request.schedule.ScheduleControllerRequest;
 import com.example.studyschedule.repository.schedule.ScheduleRepository;
 import jakarta.persistence.EntityNotFoundException;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class ScheduleServiceTest extends TestHelper {
 
@@ -66,14 +75,16 @@ class ScheduleServiceTest extends TestHelper {
     }
 
     @Test
-    @DisplayName("스케줄이 정상적으로 생성된다.")
-    void createSchedule() {
+    @Tag("long term")
+    @DisplayName("기간 타입의 스케줄을 정상적으로 생성된다.")
+    void createScheduleByLongTermType() {
         // given
         Study simpleStudy = studyHelper.createStudyWithStudyMember(member);
         ScheduleControllerRequest.CreateScheduleRequest request = ScheduleControllerRequest.CreateScheduleRequest.builder()
                 .startDate(LocalDateTime.now())
                 .endDate(LocalDateTime.now().plusDays(10))
                 .isUse(IsUse.Y)
+                .scheduleType(ScheduleType.LONG_TERM)
                 .name("새로운 스케줄")
                 .studyId(simpleStudy.getId())
                 .todoList(Collections.emptyList())
@@ -87,13 +98,107 @@ class ScheduleServiceTest extends TestHelper {
                 () -> assertThat(schedule.getStartDate()).isEqualTo(request.getStartDate()),
                 () -> assertThat(schedule.getEndDate()).isEqualTo(request.getEndDate()),
                 () -> assertThat(schedule.getIsUse()).isEqualTo(request.getIsUse()),
+                () -> assertThat(schedule.getScheduleType()).isEqualTo(ScheduleType.LONG_TERM),
                 () -> assertThat(schedule.getName()).isEqualTo(request.getName()),
                 () -> assertThat(schedule.getStudy().getId()).isEqualTo(simpleStudy.getId()));
     }
 
     @Test
+    @Tag("pattern")
+    @DisplayName("패턴 타입의 스케줄을 정상적으로 생성된다.")
+    void createScheduleByPatternType() {
+        // given
+        Study simpleStudy = studyHelper.createStudyWithStudyMember(member);
+        ScheduleControllerRequest.CreateScheduleRequest request = createScheduleRequestPatternBuilder(simpleStudy)
+                .period(SchedulePeriod.DAY)
+                .build();
+
+        // when
+        Schedule schedule = scheduleService.createSchedule(request);
+
+        // then
+        assertAll(() -> assertThat(schedule.getMember()).isEqualTo(member),
+                () -> assertThat(schedule.getPeriod()).isEqualTo(SchedulePeriod.DAY),
+                () -> assertThat(schedule.getIsUse()).isEqualTo(request.getIsUse()),
+                () -> assertThat(schedule.getScheduleType()).isEqualTo(ScheduleType.PATTERN),
+                () -> assertThat(schedule.getName()).isEqualTo(request.getName()),
+                () -> assertThat(schedule.getStudy().getId()).isEqualTo(simpleStudy.getId()));
+    }
+
+    @ParameterizedTest
+    @MethodSource("nextScheduleDate")
+    @Tag("pattern")
+    @DisplayName("SchedulePeriod 설정에 따라 다음 날짜가 지정된다.")
+    void getNextScheduleDateBySchedulePeriodType(SchedulePeriod schedulePeriod, LocalDate nextDate) {
+        // given
+        Study simpleStudy = studyHelper.createStudyWithStudyMember(member);
+        ScheduleControllerRequest.CreateScheduleRequest request = createScheduleRequestPatternBuilder(simpleStudy)
+                .period(schedulePeriod)
+                .build();
+
+        // when
+        Schedule schedule = scheduleService.createSchedule(request);
+
+        // then
+        assertThat(schedule.getNextScheduleDate().toLocalDate().isEqual(nextDate)).isTrue();
+    }
+
+    static Stream<Arguments> nextScheduleDate() {
+        return Stream.of(arguments(SchedulePeriod.DAY, LocalDateTime.now().plusDays(1).toLocalDate()),
+                arguments(SchedulePeriod.WEEK, LocalDateTime.now().plusWeeks(1).toLocalDate()),
+                arguments(SchedulePeriod.MONTH, LocalDateTime.now().plusMonths(1).toLocalDate()),
+                arguments(SchedulePeriod.YEAR, LocalDateTime.now().plusYears(1).toLocalDate()));
+    }
+
+    @Test
+    @Tag("pattern")
+    @DisplayName("SchedulePeriod가 CUSTOM 경우, 정해진 일자 만큼으로 다음 날짜가 지정된다.")
+    void schedulePeriodIsCustom() {
+        // given
+        Study simpleStudy = studyHelper.createStudyWithStudyMember(member);
+        ScheduleControllerRequest.CreateScheduleRequest request = createScheduleRequestPatternBuilder(simpleStudy)
+                .period(SchedulePeriod.CUSTOM)
+                .customDay(10L)
+                .build();
+
+        // when
+        Schedule schedule = scheduleService.createSchedule(request);
+
+        // then
+        assertThat(schedule.getNextScheduleDate().toLocalDate().isEqual(LocalDate.now().plusDays(10))).isTrue();
+    }
+    @Test
+    @Tag("pattern")
+    @DisplayName("SchedulePeriod를 WEEK로 설정하면 다음 주가 예약일로 지정된다.")
+    void schedulePeriodIsWEEK() {
+        // given
+        Study simpleStudy = studyHelper.createStudyWithStudyMember(member);
+        ScheduleControllerRequest.CreateScheduleRequest request = createScheduleRequestPatternBuilder(simpleStudy)
+                .period(SchedulePeriod.WEEK)
+                .build();
+
+        // when
+        Schedule schedule = scheduleService.createSchedule(request);
+
+        // then
+        assertThat(schedule.getNextScheduleDate()).isEqualTo(LocalDateTime.now().plusWeeks(1));
+    }
+
+
+
+    private ScheduleControllerRequest.CreateScheduleRequest.CreateScheduleRequestBuilder createScheduleRequestPatternBuilder(Study simpleStudy) {
+        return ScheduleControllerRequest.CreateScheduleRequest.builder()
+
+                .isUse(IsUse.Y)
+                .scheduleType(ScheduleType.PATTERN)
+                .name("새로운 스케줄")
+                .studyId(simpleStudy.getId())
+                .todoList(Collections.emptyList());
+    }
+
+    @Test
     @DisplayName("시작 일자가 종료 일자보다 큰 경우 예외가 발생한다.")
-    void rejectcreateScheduleWhenStartDateMoreThanEndDate() {
+    void rejectCreateScheduleWhenStartDateMoreThanEndDate() {
         // given
         Study simpleStudy = studyHelper.createStudyWithStudyMember(member);
         ScheduleControllerRequest.CreateScheduleRequest request = ScheduleControllerRequest.CreateScheduleRequest.builder()
@@ -122,6 +227,7 @@ class ScheduleServiceTest extends TestHelper {
                 .startDate(LocalDateTime.now())
                 .endDate(LocalDateTime.now().plusDays(10))
                 .isUse(IsUse.Y)
+                .scheduleType(ScheduleType.LONG_TERM)
                 .name("새로운 스케줄")
                 .studyId(simpleStudy.getId())
                 .todoList(todoIdList)
