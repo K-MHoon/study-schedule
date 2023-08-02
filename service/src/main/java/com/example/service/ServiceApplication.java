38d2ff49630589fa9f805a575b7a48770834853e
K-MHOON <azmria117@gmail.com>
2023-08-02 22:48:58 +0900

    fix : 컴포넌트 스캔 범위 수정, dependency 수정

--- service/src/main/java/com/example/service/ServiceApplication.java
+++ service/src/main/java/com/example/service/ServiceApplication.java
@@ -3,10 +3,8 @@ package com.example.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class, scanBasePackages = "com.example.common")
-@EnableJpaAuditing
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class, scanBasePackages = {"com.example"})
 public class ServiceApplication {
 
     public static void main(String[] args) {
