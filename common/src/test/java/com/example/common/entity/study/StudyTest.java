d03b96671601e67c97626230e9dd0cf06ae3ce91
K-MHOON <azmria117@gmail.com>
2023-08-18 22:17:45 +0900

    test : 비밀 스터디 변경 단위 테스트 추가

---
+++ common/src/test/java/com/example/common/entity/study/StudyTest.java
@@ -0,0 +1,56 @@
+package com.example.common.entity.study;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudyTest {
+
+    @Test
+    @DisplayName("비밀 스터디로 전환한다.")
+    void changeToPrivate() {
+        // given
+        Study study = Study.builder()
+                .secret(Boolean.FALSE)
+                .build();
+
+        // when
+        study.changeToPrivate("testPassword");
+
+        // then
+        assertThat(study)
+                .extracting("secret", "password")
+                .contains(Boolean.TRUE, "testPassword");
+    }
+
+    @Test
+    @DisplayName("비밀 스터디는 비밀 스터디로 변환하지 못한다.")
+    void changeToPrivateSecret() {
+        // given
+        Study study = Study.builder()
+                .secret(Boolean.TRUE)
+                .build();
+
+        // when // then
+        assertThatThrownBy(() -> study.changeToPrivate("testPassword"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("해당 스터디는 이미 비밀 스터디 입니다.");
+    }
+
+    @Test
+    @DisplayName("비밀 스터디는 반드시 비밀번호가 입력되어야 한다.")
+    void changeToPrivateNonePassword() {
+        // given
+        Study study = Study.builder()
+                .secret(Boolean.FALSE)
+                .build();
+
+        // when // then
+        assertThatThrownBy(() -> study.changeToPrivate(null))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("비밀 스터디에는 반드시 비밀번호가 포함되어야 합니다.");
+    }
+}
