1fd66c08cd128f2b0e071f6e9f72887ec526b5a5
K-MHOON <azmria117@gmail.com>
2023-09-19 21:04:12 +0900

    feat : StudyRegisterControllerRequest DTO validation 메시지 적용

--- service/src/main/java/com/example/service/controller/request/study/StudyRegisterControllerRequest.java
+++ service/src/main/java/com/example/service/controller/request/study/StudyRegisterControllerRequest.java
@@ -1,7 +1,7 @@
 package com.example.service.controller.request.study;
 
 import com.example.service.service.study.request.StudyRegisterServiceRequest;
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import org.hibernate.validator.constraints.UniqueElements;
@@ -17,10 +17,10 @@ public class StudyRegisterControllerRequest {
     @AllArgsConstructor
     public static final class CreateStudyRegister {
 
-        @NotEmpty
+        @NotBlank(message = "{register.goal.not-blank}")
         private String goal;
 
-        @NotEmpty
+        @NotBlank(message = "{register.objective.not-blank}")
         private String objective;
 
         private String comment;
@@ -40,8 +40,8 @@ public class StudyRegisterControllerRequest {
     @AllArgsConstructor
     public static class CancelStudyRegister {
 
-        @UniqueElements
-        @NotNull
+        @NotNull(message = "{common.list.not-null}")
+        @UniqueElements(message = "{common.list.unique-elements}")
         private List<Long> studyRegisterList;
 
         public StudyRegisterServiceRequest.CancelStudyRegister toServiceRequest() {
